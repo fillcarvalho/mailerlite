394ce5db28a9fe6012185937748212d9adcc281f
Filipe Costa <filipe@rhizatech.com.br>
2025-01-16 15:30:48 -0300

    Tailwinnd // Setting the primary color with the mailerlite green

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    colors: {
+      primary: '#09c269'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
